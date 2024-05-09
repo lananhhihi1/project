ed62ffeff2443fc56b5694979137bd89f800c2c1
lananhhihi1 <factormiaupun0suy@hotmail.com>
2024-05-09 19:58:10 +0700

    update sdk project

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-function levelOrderBottom(root) {
-  if (!root) return [];
-  const result = [];
-  const queue = [root];
-  while (queue.length) {
-    const levelSize = queue.length;
-    const currentLevel = [];
-    for (let i = 0; i < levelSize; i++) {
-      const node = queue.shift();
-      currentLevel.push(node.val);
-      if (node.left) queue.push(node.left);
-      if (node.right) queue.push(node.right);
+function maximalSquare(matrix) {
+  if (matrix.length === 0 || matrix[0].length === 0) return 0;
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  const dp = Array.from({ length: rows + 1 }, () =>
+    Array.from({ length: cols + 1 }, () => 0),
+  );
+  let maxSquare = 0;
+  for (let i = 1; i <= rows; i++) {
+    for (let j = 1; j <= cols; j++) {
+      if (matrix[i - 1][j - 1] === "1") {
+        dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1;
+        maxSquare = Math.max(maxSquare, dp[i][j]);
+      }
     }
-    result.unshift(currentLevel);
   }
-  return result;
+  return maxSquare * maxSquare;
 }
