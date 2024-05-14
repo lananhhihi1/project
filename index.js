81fccfd433dc7ff13d3022985e3af5383a560715
lananhhihi1 <factormiaupun0suy@hotmail.com>
2024-05-14 17:56:06 +0700

    hello world

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-function reverseList(head) {
-  let prev = null;
-  let curr = head;
-  while (curr) {
-    const next = curr.next;
-    curr.next = prev;
-    prev = curr;
-    curr = next;
+function findMaxLength(nums) {
+  const map = new Map();
+  map.set(0, -1);
+  let count = 0;
+  let maxLength = 0;
+  for (let i = 0; i < nums.length; i++) {
+    count += nums[i] === 1 ? 1 : -1;
+    if (map.has(count)) {
+      maxLength = Math.max(maxLength, i - map.get(count));
+    } else {
+      map.set(count, i);
+    }
   }
-  return prev;
+  return maxLength;
 }
